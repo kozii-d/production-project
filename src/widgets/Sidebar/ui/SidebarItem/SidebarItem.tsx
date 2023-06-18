4cb08d488e822398f0904756be15483873e64968
Dmitrii Kozii <dkozii1807@gmail.com>
2023-06-18 16:36:29 +0300

    fix sidebar

--- src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -5,8 +5,8 @@ import { memo } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useSelector } from "react-redux";
 import { getUserAuthData } from "entities/User";
+import { SidebarItemType } from "../../model/types/sidebar";
 import cls from "./SidebarItem.module.scss";
-import { SidebarItemType } from "../../model/iItems";
 
 interface SidebarItemProps {
   item: SidebarItemType;
